a48d033d9d3e99b491045875047a74353015b34e
Josue0625 <jcampos@ufpso.edu.co>
2022-12-16 09:31:50 -0500

    Correciones en los mensajes

--- front_api_procesos_negocio/js/articulo.js
+++ front_api_procesos_negocio/js/articulo.js
@@ -1,20 +1,20 @@
 //funciones js para el modulo de articulos
 
 function listarArticulos() {
-  validaToken();
-  var cont = 0;
-  var settings = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: localStorage.token,
-    },
-  };
-  fetch(urlApi + "/articulos", settings)
-    .then((response) => response.json())
-    .then(function (data) {
-      var articulos = `
+    validaToken();
+    var cont = 0;
+    var settings = {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: localStorage.token,
+        },
+    };
+    fetch(urlApi + "/articulos", settings)
+        .then((response) => response.json())
+        .then(function(data) {
+            var articulos = `
             <div class="p-3 mb-2 bg-light text-dark">
                     <h1 class="display-5"><i class="fa-solid fa-list"></i> Listado de articulos</h1>
                 </div>
@@ -30,9 +30,9 @@ function listarArticulos() {
                         </tr>
                     </thead>
                     <tbody id="listar">`;
-      for (const articulo of data) {
-        cont++;
-        articulos += `
+            for (const articulo of data) {
+                cont++;
+                articulos += `
                         <tr>
                             <th scope="row">${cont}</th>
                             <td>${articulo.codigo}</td>
@@ -52,59 +52,59 @@ function listarArticulos() {
                             </td>
                         </tr>
                     `;
-      }
-      articulos += `
+            }
+            articulos += `
             </tbody>
                 </table>
             `;
-      document.getElementById("datos").innerHTML = articulos;
-    })
-    .catch(function (error) {
-      var agregar = `<a href="#" onclick="registerFormA('true')" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></a>`;
-      document.getElementById("datos").innerHTML = agregar;
-      alertas("La base de datos esta vasia!", 2);
-    });
+            document.getElementById("datos").innerHTML = articulos;
+        })
+        .catch(function(error) {
+            var agregar = `<a href="#" onclick="registerFormA('true')" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></a>`;
+            document.getElementById("datos").innerHTML = agregar;
+            alertas("La tabla donde se almacenan los art&iacute;culos esta vac&iacute;a!", 2);
+        });
 }
 
 function eliminaArticulo(codigo) {
-  validaToken();
-  var settings = {
-    method: "DELETE",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: localStorage.token,
-    },
-  };
-  fetch(urlApi + "/articulo/" + codigo, settings).then((data) => {
-    console.log(data); // JSON data parsed by `data.json()` call
-    listarArticulos();
-    alertas("Se ha eliminado el articulo exitosamente!", 2);
-  });
+    validaToken();
+    var settings = {
+        method: "DELETE",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: localStorage.token,
+        },
+    };
+    fetch(urlApi + "/articulo/" + codigo, settings).then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        listarArticulos();
+        alertas("Se ha eliminado el articulo exitosamente!", 2);
+    });
 }
 
 function verModificarArticulo(codigo) {
-  console.log(codigo);
-  validaToken();
-  var settings = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: localStorage.token,
-    },
-  };
+    console.log(codigo);
+    validaToken();
+    var settings = {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: localStorage.token,
+        },
+    };
 
-  fetch(urlApi + "/articulo/" + codigo, settings)
-    .then((response) => response.json())
-    .then(function (articulo) {
-      if (articulo) {
-        fetch(urlApi + "/categorias", settings)
-          .then((resp) => resp.json())
-          .then(function (da) {
-            var cadena = "";
-            var fechaDB = articulo.fecha;
-            cadena = `
+    fetch(urlApi + "/articulo/" + codigo, settings)
+        .then((response) => response.json())
+        .then(function(articulo) {
+            if (articulo) {
+                fetch(urlApi + "/categorias", settings)
+                    .then((resp) => resp.json())
+                    .then(function(da) {
+                        var cadena = "";
+                        var fechaDB = articulo.fecha;
+                        cadena = `
                 <div class="p-3 mb-2 bg-light text-dark">
                     <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Modificar articulo</h1>
                 </div>
@@ -122,7 +122,7 @@ function verModificarArticulo(codigo) {
                     <input type="text" class="form-control" name="descripcion" id="descripcion" required value="${
                       articulo.descripcion
                     }"> <br>
-                    <label for="fecha"  class="form-label">Fecha de publicaci&0acute;n</label>
+                    <label for="fecha"  class="form-label">Fecha de publicaci&oacute;n</label>
                     <input type="date" class="form-control" name="fecha" id="fecha" required value="${fechaDB.substr(
                       0,
                       10
@@ -144,13 +144,13 @@ function verModificarArticulo(codigo) {
                       <option value="${articulo.categoria.id}">${
               articulo.categoria.nombre
             }</option>`;
-            for (const category of da) {
-              if (articulo.categoria.id != category.id) {
-                cadena += `
+                        for (const category of da) {
+                            if (articulo.categoria.id != category.id) {
+                                cadena += `
                         <option value="${category.id}">${category.nombre}</option>`;
-              }
-            }
-            cadena += `
+                            }
+                        }
+                        cadena += `
                     </select>
                     </br>
                     <label for="user" class="form-label">Craedor del Art&iacute;culo</label>
@@ -159,64 +159,64 @@ function verModificarArticulo(codigo) {
                     <button type="button" class="btn btn-outline-warning" onclick="modificarArticulo('${articulo.codigo}')">Modificar
                     </button>
                 </form>`;
-            document.getElementById("contentModal").innerHTML = cadena;
-            var myModal = new bootstrap.Modal(
-              document.getElementById("modalUsuario")
-            );
-            myModal.toggle();
-          });
-      }
-    });
+                        document.getElementById("contentModal").innerHTML = cadena;
+                        var myModal = new bootstrap.Modal(
+                            document.getElementById("modalUsuario")
+                        );
+                        myModal.toggle();
+                    });
+            }
+        });
 }
 
 async function modificarArticulo(codigo) {
-  validaToken();
-  var myForm = document.getElementById("myFormA");
-  var formData = new FormData(myForm);
-  var jsonData = {};
-  var jsonCategoria = {};
-  for (var [k, v] of formData) {
-    //convertimos los datos a json
-    if (k == "categoria") {
-      jsonCategoria["id"] = v;
-      jsonData[k] = jsonCategoria;
-    } else {
-      jsonData[k] = v;
+    validaToken();
+    var myForm = document.getElementById("myFormA");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    var jsonCategoria = {};
+    for (var [k, v] of formData) {
+        //convertimos los datos a json
+        if (k == "categoria") {
+            jsonCategoria["id"] = v;
+            jsonData[k] = jsonCategoria;
+        } else {
+            jsonData[k] = v;
+        }
     }
-  }
-  const request = await fetch(urlApi + "/articulo/" + codigo, {
-    method: "PUT",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: localStorage.token,
-    },
-    body: JSON.stringify(jsonData),
-  });
-  listarArticulos();
-  alertas("Se ha modificado el articulo exitosamente!", 1);
-  document.getElementById("contentModal").innerHTML = "";
-  var myModalEl = document.getElementById("modalUsuario");
-  var modal = bootstrap.Modal.getInstance(myModalEl); // Returns a Bootstrap modal instance
-  modal.hide();
+    const request = await fetch(urlApi + "/articulo/" + codigo, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: localStorage.token,
+        },
+        body: JSON.stringify(jsonData),
+    });
+    listarArticulos();
+    alertas("Se ha modificado el articulo exitosamente!", 1);
+    document.getElementById("contentModal").innerHTML = "";
+    var myModalEl = document.getElementById("modalUsuario");
+    var modal = bootstrap.Modal.getInstance(myModalEl); // Returns a Bootstrap modal instance
+    modal.hide();
 }
 
 function verArticulo(codigo) {
-  validaToken();
-  var settings = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: localStorage.token,
-    },
-  };
-  fetch(urlApi + "/articulo/" + codigo, settings)
-    .then((response) => response.json())
-    .then(function (articulo) {
-      var cadena = "";
-      if (articulo) {
-        cadena = `
+    validaToken();
+    var settings = {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: localStorage.token,
+        },
+    };
+    fetch(urlApi + "/articulo/" + codigo, settings)
+        .then((response) => response.json())
+        .then(function(articulo) {
+            var cadena = "";
+            if (articulo) {
+                cadena = `
                 <div class="p-3 mb-2 bg-light text-dark">
                     <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Visualizar articulo</h1>
                 </div>
@@ -248,31 +248,31 @@ function verArticulo(codigo) {
                       articulo.categoria.nombre
                     }</li>
                 </ul>`;
-      }
-      document.getElementById("contentModal").innerHTML = cadena;
-      var myModal = new bootstrap.Modal(
-        document.getElementById("modalUsuario")
-      );
-      myModal.toggle();
-    });
+            }
+            document.getElementById("contentModal").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(
+                document.getElementById("modalUsuario")
+            );
+            myModal.toggle();
+        });
 }
 
 function registerFormA(auth = false) {
-  var settings = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: localStorage.token,
-    },
-  };
-  fetch(urlApi + "/categorias", settings)
-    .then((response) => response.json())
-    .then(function (data) {
-      fetch(urlApi + "/listusers", settings)
+    var settings = {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: localStorage.token,
+        },
+    };
+    fetch(urlApi + "/categorias", settings)
         .then((response) => response.json())
-        .then(function (datauser) {
-          cadena = `
+        .then(function(data) {
+            fetch(urlApi + "/listusers", settings)
+                .then((response) => response.json())
+                .then(function(datauser) {
+                    cadena = `
             <div class="p-3 mb-2 bg-light text-dark">
                 <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Registrar articulo</h1>
             </div>
@@ -297,77 +297,77 @@ function registerFormA(auth = false) {
                 <select class="form-select" id="categoria" name="categoria" aria-label="Default select example">
                             <option value="0"></option>
                             `;
-          for (const categoria of data) {
-            console.log(categoria.id);
-            cadena += `<option value="${categoria.id}">${categoria.nombre}</option>`;
-          }
-          cadena += `
+                    for (const categoria of data) {
+                        console.log(categoria.id);
+                        cadena += `<option value="${categoria.id}">${categoria.nombre}</option>`;
+                    }
+                    cadena += `
                 </select><br>
                 <label for="user">Usuario</label>
                 <select class="form-select" id="user" name="user" aria-label="Default select example">
                             <option value="0"></option>
                             `;
-          for (const user of datauser) {
-            console.log(user.id);
-            cadena += `<option value="${user.id}">${user.name}</option>`;
-          }
-          cadena += `
+                    for (const user of datauser) {
+                        console.log(user.id);
+                        cadena += `<option value="${user.id}">${user.name}</option>`;
+                    }
+                    cadena += `
                 </select><br>
             <button type = "button" class = "btn btn-outline-info" onclick = "registrarArticulo('${auth}')" > Registrar </button> 
             </form>`;
-          document.getElementById("contentModal").innerHTML = cadena;
-          var myModal = new bootstrap.Modal(
-            document.getElementById("modalUsuario")
-          );
-          myModal.toggle();
+                    document.getElementById("contentModal").innerHTML = cadena;
+                    var myModal = new bootstrap.Modal(
+                        document.getElementById("modalUsuario")
+                    );
+                    myModal.toggle();
+                });
+        })
+        .catch(function(error) {
+            var agregar = `<a href="#" onclick="registerFormCa('true')" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></a>`;
+            document.getElementById("datos").innerHTML = agregar;
+            alertas(
+                "Agrege primero una categor&iacute;a dandole click al bot&oacute;n, ya que la base de categoria tambi&eacute;n esta vac&iacute;a!",
+                2
+            );
         });
-    })
-    .catch(function (error) {
-      var agregar = `<a href="#" onclick="registerFormCa('true')" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></a>`;
-      document.getElementById("datos").innerHTML = agregar;
-      alertas(
-        "Agrege primero una categor&iacute;a dandole click al bot&oacute;n, ya que la base de categoria tambi&eacute;n esta vac&iacute;a!",
-        2
-      );
-    });
 }
 
 async function registrarArticulo(auth = false) {
-  var myForm = document.getElementById("myFormRegA");
-  var formData = new FormData(myForm);
-  var jsonData = {};
-  var jsonCategoria = {};
-  var jsonUser = {};
-  for (var [k, v] of formData) {
-    if (k == "categoria") {
-      jsonCategoria["id"] = v;
-      jsonData[k] = jsonCategoria;
-    } else if (k == "user") {
-      jsonUser["id"] = v;
-      jsonData[k] = jsonUser;
-    } else {
-      jsonData[k] = v;
+    var myForm = document.getElementById("myFormRegA");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    var jsonCategoria = {};
+    var jsonUser = {};
+    for (var [k, v] of formData) {
+        if (k == "categoria") {
+            jsonCategoria["id"] = v;
+            jsonData[k] = jsonCategoria;
+        } else if (k == "user") {
+            jsonUser["id"] = v;
+            jsonData[k] = jsonUser;
+        } else {
+            jsonData[k] = v;
+        }
     }
-  }
-  console.log("data user ", jsonData);
-  const request = await fetch(urlApi + "/articulo", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(jsonData),
-  })
-    .then((response) => response.json())
-    .then(function (respuesta) {
-      console.log("respuesta peticion", respuesta);
-    });
-  if (auth) {
-    listarArticulos();
-  }
-  alertas("Se ha registrado el articulo exitosamente!", 1);
-  document.getElementById("contentModal").innerHTML = "";
-  var myModalEl = document.getElementById("modalUsuario");
-  var modal = bootstrap.Modal.getInstance(myModalEl); // Returns a Bootstrap modal instance
-  modal.hide();
+    console.log("data user ", jsonData);
+    const request = await fetch(urlApi + "/articulo", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(jsonData),
+        })
+        .then((response) => response.json())
+        .then(function(respuesta) {
+            console.log("respuesta peticion", respuesta);
+        });
+    if (auth) {
+        listarArticulos();
+    }
+    alertas("Se ha registrado el articulo exitosamente!", 1);
+    document.getElementById("contentModal").innerHTML = "";
+    var myModalEl = document.getElementById("modalUsuario");
+    var modal = bootstrap.Modal.getInstance(myModalEl); // Returns a Bootstrap modal instance
+    modal.hide();
 }
